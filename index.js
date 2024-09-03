25e19b1e6bddf0c237b39bd4877ed31441658ce9
Даня Назариков <81469711+nazarikovd@users.noreply.github.com>
2024-09-04 01:29:20 +0300

    eto zaliv

---
+++ index.js
@@ -0,0 +1,170 @@
+const axios = require("axios")
+const {
+	VK,
+	resolveResource,
+	Keyboard
+} = require("vk-io");
+
+
+module.exports = class Botex {
+
+	constructor(token) {
+		this.vk = null
+		this.token = token
+		this.app_id = 6983801
+		this.creds = null
+		this.bearer = null
+		this.user = null
+		this._cotexData = null
+	}
+
+	load = async () => {
+		try {
+			this.vk = new VK({
+				token: this.token
+			});
+			this.user = await this.vk.api.users.get()
+			this.user = this.user[0]
+		} catch {
+			return {
+				"success": false,
+				"error": "VK INVALID TOKEN"
+			}
+		}
+
+		await this.getCreds()
+		await this.auth()
+		return {
+			"success": true,
+			"user": this.user
+		}
+	}
+
+	getCreds = async () => {
+
+		let apps = await this.vk.api.call('apps.get', {
+			app_id: 6983801
+		});
+
+		let webview = apps.items[0].webview_url
+		let creds = webview.split("?")[1]
+		this.creds = creds
+		return creds
+
+	}
+
+	auth = async () => {
+		let auth = await axios.get("https://kotex-flow.ru-prod2.kts.studio/api/user/auth?" + this.creds)
+		this.bearer = auth.data.data.token
+		this._cotexData = auth.data.data.user
+		return auth.data.data
+	}
+
+	register = async () => {
+		let result = {}
+		let headers = {
+			"authorization": "Bearer " + this.bearer
+		}
+		let data = {
+			"value": true,
+			"name": "policy052022"
+		}
+		let flagdata = await axios.post("https://kotex-flow.ru-prod2.kts.studio/api/user/flag", data, {
+			headers: headers
+		})
+		result.flag = flagdata.data
+		let date = this.getCurrentDateFormatted()
+		data = {
+			"birthdate": "1998-10-10",
+			"email": "123@mail.ru",
+			"phone": "79999999999",
+			"cycle_date": date,
+			"period_duration": 5,
+			"cycle_duration": 28
+		}
+		let regdata = await axios.post("https://kotex-flow.ru-prod2.kts.studio/api/user/register_with_cycle", data, {
+			headers: headers
+		})
+		result.reg = regdata.data
+		return result
+
+	}
+
+	getShop = async () => {
+		let headers = {
+			"authorization": "Bearer " + this.bearer
+		}
+		let shopdata = await axios.get("https://kotex-flow.ru-prod2.kts.studio/api/shop/list", {
+			headers: headers
+		})
+		return shopdata.data
+	}
+
+	tryToGetAllPoints = async () => {
+		let results = {}
+		let headers = {
+			"authorization": "Bearer " + this.bearer
+		}
+		let data = {
+			"value": true,
+			"type": "app"
+		}
+
+		let pointsdata = await axios.post("https://kotex-flow.ru-prod2.kts.studio/api/user/set_notifications", data, {
+			headers: headers
+		})
+		results.notifications = pointsdata.data
+
+		let date = this.getCurrentDateFormatted()
+		data = {
+			"date": date,
+			"symptoms": ["tired"]
+		}
+
+		let symptomsdata = await axios.post("https://kotex-flow.ru-prod2.kts.studio/api/calendar/add_symptoms", data, {
+			headers: headers
+		})
+		results.symptoms = symptomsdata.data
+		return results
+	}
+
+	markDays = async () => {
+		let headers = {
+			"authorization": "Bearer " + this.bearer
+		}
+		let dates = this.getMarkDateFormatted()
+		let data = {
+			"unmark": [],
+			"mark": dates
+		}
+		let markdata = await axios.post("https://kotex-flow.ru-prod2.kts.studio/api/calendar/mark_days", data, {
+			headers: headers
+		})
+		return markdata.data
+	}
+
+	getCurrentDateFormatted = () => {
+		let date = new Date();
+		let year = date.getFullYear();
+		let month = String(date.getMonth() + 1).padStart(2, '0'); // Months r zerobsd, so add 1
+		let day = String(date.getDate()).padStart(2, '0');
+		return `${year}-${month}-${day}`;
+	}
+
+	getMarkDateFormatted = () => {
+		let dates = [];
+		let date = new Date();
+
+		for (let i = 0; i < 5; i++) {
+			let futureDate = new Date(date);
+			futureDate.setDate(date.getDate() + i);
+			let year = futureDate.getFullYear();
+			let month = String(futureDate.getMonth() + 1).padStart(2, '0'); // Months r zerobsd, so add 1
+			let day = String(futureDate.getDate()).padStart(2, '0');
+			dates.push(`${year}-${month}-${day}`);
+		}
+
+		return dates;
+	};
+
+}
---
+++ server.js
@@ -0,0 +1,155 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const Botex = require('./index.js')
+const {
+	randomUUID
+} = require('crypto')
+let bots = []
+
+
+
+app.get('/accounts.add', (req, res) => {
+
+	let token = req.query.token
+
+	if (!token) {
+		return res.status(400).send('Token is required');
+	}
+	let uuid = randomUUID()
+	let botex = new Botex(token);
+	botex.load().then((state) => {
+		if (state.success) {
+
+			bots.push({
+				"uuid": uuid,
+				"botex": botex
+			})
+
+			res.send({
+				"result": state
+			})
+
+		} else {
+
+			res.send({
+				"result": state
+			})
+
+		}
+
+	});
+
+});
+
+app.get('/accounts.getAll', (req, res) => {
+	let result = []
+
+	for (let bot of bots) {
+		result.push({
+			"uuid": bot.uuid,
+			"uid": bot.botex.user.id,
+			"points": bot.botex._cotexData.points,
+			"_lastCotexData": bot.botex._cotexData
+		})
+	}
+
+	res.send({
+		"count": bots.length,
+		"bots": result
+	})
+
+
+});
+
+app.get('/cotex.getShop', (req, res) => {
+	let uuid = req.query.uuid
+	let uid = req.query.uid
+
+	if (!uuid && !uid) {
+		bots[0].botex.getShop().then((a) => {
+			res.send({
+				"data": a
+			})
+		})
+
+	} else {
+
+		if (uuid && uid) {
+			res.send("error only one of uuid uid can be set")
+			return
+		}
+
+		let bot
+
+		if (uuid) {
+			bot = bots.filter((bott) => bott.uuid == uuid)
+		}
+		if (uid) {
+			bot = bots.filter((bott) => bott.botex.user.id == uid)
+		}
+		console.log(bot, bots)
+		if (!bot[0]) {
+			res.send("not found")
+			return
+		}
+		bot[0].botex.getShop().then((a) => {
+			res.send({
+				"data": a
+			})
+		})
+
+
+	}
+});
+
+app.get('/cotex.tryToGetAllPoints', async (req, res) => {
+	let uuid = req.query.uuid
+	let uid = req.query.uid
+
+	if (!uuid && !uid) {
+
+		let results = []
+
+		for (let bot of bots) {
+			let res = await bot.botex.tryToGetAllPoints()
+			results.push(res)
+		}
+
+		res.send({
+			"results": results
+		})
+
+	} else {
+
+		if (uuid && uid) {
+			res.send("error only one of uuid uid can be set")
+			return
+		}
+
+		let bot
+
+		if (uuid) {
+			bot = bots.filter((bott) => bott.uuid == uuid)
+		}
+		if (uid) {
+			bot = bots.filter((bott) => bott.botex.user.id == uid)
+		}
+		console.log(bot, bots)
+		if (!bot[0]) {
+			res.send("not found")
+			return
+		}
+		bot[0].botex.tryToGetAllPoints().then((a) => {
+			res.send({
+				"data": a
+			})
+		})
+
+
+	}
+});
+
+app.listen(port, () => {
+	console.log(`runnin on http://localhost:${port}`)
+})
